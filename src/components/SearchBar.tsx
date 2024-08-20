99ba6270d44995bc04184fa5750ec5ceb3a96534
Devi <diptikadevi@gmail.com>
2024-08-20 14:58:16 -0400

    small clean up

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,9 @@
-import * as React from 'react';
 import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 
 const Search = styled('div')(({ theme }) => ({
@@ -54,8 +51,6 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 export default function SearchAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
 
           <Typography
             variant="h6"
@@ -73,8 +68,7 @@ export default function SearchAppBar() {
               inputProps={{ 'aria-label': 'search' }}
             />
           </Search>
-        </Toolbar>
-      </AppBar>
+
     </Box>
   );
 }
