8746230e671ee731cad3719309f8fa8b128d2765
Devi <diptikadevi@gmail.com>
2024-08-19 21:57:40 -0400

    page works

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, createTheme, CssBaseline, Container } from '@mui/material';
+import AppNavbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import SearchAppBar from './components/SearchBar';
 
-function App() {
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ccb9f0', 
+    },
+    secondary: {
+      main: '#92d49f',
+    },
+    background: {
+      default: '#f0dfb9',
+    },
+  },
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    h4: {
+      fontWeight: 600,
+    },
+  },
+});
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppNavbar />
+      <Container maxWidth="lg">
+      <SearchAppBar/>
+        <HomePage />
+      </Container>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
